166d04a4bc67235a83a6e49d74812e612a2ced18
Aniekeme <mercysam299@gmail.com>
2022-04-19 18:48:24 +0100

    Hey

---
+++ script.js
@@ -0,0 +1,13 @@
+let quote ="If you believe, nothing is impossible";
+
+let x ="42" + 7;
+console.log(x);
+
+let x ="42" - 7;
+console.log(x);
+
+let coding ="cool";
+console.log(coding);
+
+let coding = "trendy and fun"
+console.log(coding);
